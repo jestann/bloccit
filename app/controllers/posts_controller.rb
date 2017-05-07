a6e4b7d70f8803c6a23dfbfdb1e01b22fd0faf2f
jestann <jessannbird@gmail.com>
2017-05-07 07:46:41 +0000

    A18: CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    
+    # censor every fifth post
+    count = 1
+    @posts.each do |post|
+      if count % 5 == 0
+        post.title = "SPAM"
+      end
+      count += 1
+    end
+    
   end
 
   def show
