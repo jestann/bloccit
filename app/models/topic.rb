60ea2d753d3d859899e66fb891fde0bf69414a6d
jestann <jessannbird@gmail.com>
2017-05-16 20:05:44 +0000

    A-22: Validation and Partials for Topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,10 @@
 class Topic < ActiveRecord::Base
     has_many :posts, dependent: :destroy
     # when a topic is destroyed, its associated posts should die
+    
+    validates :name, length: {minimum: 5}, presence: true
+    validates :description, length: {minimum: 15}, presence: true
+    # validates :public, presence: true
+    # this uses a checkbox which will always supply "public"
+    # validating its presence forces it to never be unchecked
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -7,6 +7,17 @@ RSpec.describe Topic, type: :model do
     let(:topic) { Topic.create!(name: name, description: description) }
 
     it { is_expected.to have_many(:posts) }
+    
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+    # it { is_expected.to validate_presence_of(:public) }
+    # can't validate this, as it uses a checkbox
+    # leaving it unchecked = blank and the form won't submit
+    # so thus public can never be false
+
+    it { is_expected.to validate_length_of(:name).is_at_least(5) }
+    it { is_expected.to validate_length_of(:description).is_at_least(15) }
+
 
     describe "attributes" do
         it "has name, description, public attributes" do
